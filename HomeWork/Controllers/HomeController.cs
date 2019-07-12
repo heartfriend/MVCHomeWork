44632ee1d621f884ed5aec277b461223cd7689d3
Lin Hsin I <heartfriend3397@gmail.com>
2019-07-12 13:16:04 +0800

    20190712_hw

--- HomeWork/Controllers/HomeController.cs
+++ HomeWork/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeWork.Models.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,18 @@ namespace HomeWork.Controllers
 			ViewBag.Message = "Your contact page.";
 
 			return View();
+		}
+
+		public ActionResult Bill()
+		{
+			List<BillViewModel> BillList = new List<BillViewModel>();
+			BillList.Add(new BillViewModel { ID = "1", TYPE = "收入", DATE = "2019-07-01", AMT = "3,100", });
+			BillList.Add(new BillViewModel { ID = "2", TYPE = "支出", DATE = "2019-07-05", AMT = "150", });
+			BillList.Add(new BillViewModel { ID = "3", TYPE = "支出", DATE = "2019-07-05", AMT = "200", });
+			BillList.Add(new BillViewModel { ID = "4", TYPE = "收入", DATE = "2019-07-12", AMT = "1,000", });
+
+			return View(BillList.ToList());
+
 		}
 	}
 }
---
+++ HomeWork/Models/ViewModels/BillViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork.Models.ViewModel
+{
+	public class BillViewModel
+	{
+		[DisplayName("#")]
+		public string ID { get; set; }
+
+		[DisplayName("類別")]
+		public string TYPE { get; set; }
+
+		[DisplayName("日期")]
+		public string DATE { get; set; }
+
+		[DisplayName("金額")]
+		public string AMT { get; set; }
+
+	}
+}
